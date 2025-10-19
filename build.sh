a849ddf7f938ee290d6b66e2e1f2518729b09aab
Prakul-bajaj <bajajprakul083@gmail.com>
2025-10-19 20:28:34 +0530

    corrected manage.py path issue during deployment

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -o errexit
 pip install -r requirements_2.txt
 pip install djongo==1.3.6 --no-deps
 pip install -r requirements_1.txt
+cd fixmate-backend
 python manage.py collectstatic --no-input
